c9844ff8d88556ef35416c42ca53a971627a6b15
Blessing Nyaberi Hamisi <nyaberihamisi@gmail.com>
2025-07-18 21:19:41 +0300

    update

--- lib/views/inhouse_sales_summary_page.dart
+++
@@ -1,9 +0,0 @@
-import 'views/inhouse_sales_summary_page.dart';
-
-ElevatedButton(
-  onPressed: () => Navigator.push(
-    context,
-    MaterialPageRoute(builder: (_) => const InhouseSalesSummaryPage()),
-  ),
-  child: const Text('In-House Sales Summary'),
-),
